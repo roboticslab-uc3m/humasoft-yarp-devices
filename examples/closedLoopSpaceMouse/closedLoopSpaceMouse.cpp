2a67599364d8222cd701fc101c2adb5248ea5efc
Raul de Santos Rico <rasantos@it.uc3m.es>
2020-01-22 17:37:30 +0100

    added decodePose to see the result in polar & azimuth

--- examples/closedLoopSpaceMouse/closedLoopSpaceMouse.cpp
+++ examples/closedLoopSpaceMouse/closedLoopSpaceMouse.cpp
@@ -23,9 +23,11 @@
 #include <yarp/dev/IAnalogSensor.h>
 
 #include <ICartesianControl.h> // we need this to work with the CartesianControlClient device
+#include <KinematicRepresentation.hpp> // encodePose, decodePose
 
 #include <ColorDebug.h>
 
+using namespace roboticslab::KinRepresentation;
 
 int main(int argc, char *argv[])
 {
@@ -62,19 +64,37 @@ int main(int argc, char *argv[])
     CD_SUCCESS("Acquired interface [ok]\n");
 
 
-    yarp::sig::Vector values;
+    int channels = iAnalogSensor->getChannels();
+
+    if(channels==0)
+    {
+        CD_ERROR("Failed number of channels\n");
+        return 1;
+    }
+
+    yarp::sig::Vector mouseValues;
+    std::vector<double> sumValues, outValues;
+    sumValues.resize(channels);
 
     while(1)
     {
-        if(iAnalogSensor->read(values) != yarp::dev::IAnalogSensor::AS_OK)
+        if(iAnalogSensor->read(mouseValues) != yarp::dev::IAnalogSensor::AS_OK)
         {
             CD_ERROR("not values\n");
             break;
         }
 
-        for(int i=0; i< values.size(); i++)
-            printf("%f ",values[i]);
-        printf("\n");
+        // suma
+        for(int i=0; i< mouseValues.size(); i++)
+            sumValues[i] += mouseValues[i];
+
+        if (!decodePose(sumValues, outValues, coordinate_system::NONE, orientation_system::POLAR_AZIMUTH, angular_units::DEGREES))
+        {
+            CD_ERROR("decodePose failed.\n");
+            return false;
+        }
+
+        printf("Inclinacion (%f) Orientacion (%f)\n", outValues[0], outValues[1]);
 
         yarp::os::Time::delay(0.005);
     }
