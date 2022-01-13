8f450091a1610b1bcc2bdf03a94fdb3b17e36e31
Raúl de Santos Rico <rasantos@it.uc3m.es>
2022-01-13 13:22:53 +0100

    created new CartesianControlClient example to test a softArm trajectory using movi

---
+++ examples/cpp/softArmCartesianControlClient/exampleCartesianControlClient.cpp
@@ -0,0 +1,93 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/**
+ * @ingroup humasoft-yarp-devices-examples
+ * \defgroup exampleCartesianControlClient exampleCartesianControlClient
+ *
+ * <b>Legal</b>
+ *
+ * Copyright: (C) 2022 Universidad Carlos III de Madrid;
+ *
+ * Authors: Raul de Santos Rico
+ *
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see license/LGPL.TXT
+ *
+ */
+
+#include <yarp/os/LogStream.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Property.h>
+
+#include <yarp/dev/PolyDriver.h>
+
+#include <ICartesianControl.h>
+
+int main(int argc, char *argv[])
+{
+    yarp::os::Network yarp;
+
+    if (!yarp::os::Network::checkNetwork())
+    {
+        yError() << "Please start a yarp name server first";
+        return 1;
+    }
+
+    yarp::os::Property options {
+        {"device", yarp::os::Value("CartesianControlClient")},
+        {"cartesianRemote", yarp::os::Value("/SoftArmControl/rpc_transform")},
+        {"cartesianLocal", yarp::os::Value("/SoftArmCartesianControlClient")}
+    };
+
+    yarp::dev::PolyDriver dd(options);
+
+    if (!dd.isValid())
+    {
+        yError() << "Device not available";
+        return 1;
+    }
+
+    roboticslab::ICartesianControl *iCartesianControl;
+
+    if (!dd.view(iCartesianControl))
+    {
+        yError() << "Problems acquiring interface";
+        return 1;
+    }
+
+
+    yInfo() << "Step 1: poss (10 0)";
+
+    if (!iCartesianControl->movj({10,0}))
+    {
+        yError() << "failure";
+        return 1;
+    }
+
+    iCartesianControl->wait(3);
+
+    yInfo() << "Step 2: make a circumference";
+    
+    double period = 0.01; // 10ms
+    iCartesianControl->setParameter(VOCAB_CC_CONFIG_CMC_PERIOD, period);
+    iCartesianControl->setParameter(VOCAB_CC_CONFIG_STREAMING_CMD, VOCAB_CC_MOVI);
+    
+    for(double ori=0.0; ori<360; ori+=0.1)
+    {
+        yInfo() << ori ;
+        iCartesianControl->movi({10, ori});
+        yarp::os::Time::delay(period);
+    }
+
+    yInfo() << "Step 3: homing";
+
+    if (!iCartesianControl->movj({0, 0}))
+    {
+        yError() << "failure";
+        return 1;
+    }
+
+    iCartesianControl->wait(3);
+
+    dd.close();
+    return 0;
+}
