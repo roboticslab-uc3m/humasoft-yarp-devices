68d3df6875cd79a3831d99a1415aa9cbf83052a4
Raúl de Santos Rico <rasantos@it.uc3m.es>
2022-03-17 13:51:46 +0100

    changed name port

--- libraries/YarpPlugins/Imu/Imu.cpp
+++ libraries/YarpPlugins/Imu/Imu.cpp
@@ -6,8 +6,8 @@ void IMUdevice::setupIMU() {
     sensor = new IMU3DMGX510(comport, DEFAULT_FREQUENCY); //Main key changing constructor of IMU3DMGX510 atribute string --> string portname = "..."
     if (sensor->check())
     {
-        yarpPort.open(nameyarpoutport+"/sensor:o");
-        cout << "Yarp port: " << nameyarpoutport+"/sensor:o" << " has been correctly opened." << endl;
+        yarpPort.open(nameyarpoutport+"/out");
+        cout << "Yarp port: " << nameyarpoutport+"/out" << " has been correctly opened." << endl;
     }
     sensor->set_freq(frequency);
     //Once the device is correctly connected, it's set to IDLE mode to stop transmitting data till user requests it
